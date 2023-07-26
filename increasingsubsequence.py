b3afd067fc82f7cf3ec20353923404e0a951616f
Lukeman Nouri <100743007+NNLuke@users.noreply.github.com>
2023-07-26 09:10:15 -0400

    Add files via upload

--- increasingsubsequence.py
+++ increasingsubsequence.py
@@ -33,7 +33,6 @@ Input: [1, 3, 5, 7, 9] Output: 5
 
 Input: [3, 1, 4, 1, 5, 9, 2, 6, 5], Output: 4
 
-
 """
 class Solution:
     def find_longest_increasing_subsequence(self, arr):
@@ -43,8 +42,6 @@ class Solution:
             for i in range(len(arr)-1):
                  if arr[i]<arr[i+1]:
                       value += 1
-            if value == 0:
-                 value = 1
             return value
             pass
 
