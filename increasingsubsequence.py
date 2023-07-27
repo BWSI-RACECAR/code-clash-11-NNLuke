714280686e40c413accb06bfbcb526b9c6a49ef2
Lukeman Nouri <100743007+NNLuke@users.noreply.github.com>
2023-07-26 22:41:22 -0400

    Add files via upload

--- increasingsubsequence.py
+++ increasingsubsequence.py
@@ -44,7 +44,7 @@ class Solution:
                 if largest_val<arr[i]:
                     value += 1
                     largest_val = arr[i]
-            return arr
+            return value
 
 
 def main():
